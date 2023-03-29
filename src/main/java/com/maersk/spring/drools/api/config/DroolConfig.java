cdf886d745dec910a12b0d73a3ad32095bc353af
Sambit Samaya Sarangi <99239146+Sambit7080@users.noreply.github.com>
2023-03-29 18:33:16 +0530

    Add files via upload

---
+++ src/main/java/com/maersk/spring/drools/api/config/DroolConfig.java
@@ -0,0 +1,54 @@
+package com.maersk.spring.drools.api.config;
+
+import java.io.IOException;
+
+import org.kie.api.KieServices;
+import org.kie.api.builder.*;
+import org.kie.api.io.ResourceType;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.io.ResourceFactory;
+import org.kie.internal.utils.KieHelper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DroolConfig {
+
+	private KieServices kieServices = KieServices.Factory.get();
+
+	private KieFileSystem getKieFileSystem() throws IOException {
+		KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
+		//kieFileSystem.write(ResourceFactory.newClassPathResource("Order.drl"));
+		kieFileSystem.write(ResourceFactory.newClassPathResource("Order.xls"));
+		return kieFileSystem;
+
+	}
+
+	@Bean
+	public KieContainer getKieContainer() throws IOException {
+		getKieRepository();
+		KieBuilder kb = kieServices.newKieBuilder(getKieFileSystem());
+		kb.buildAll();
+		KieModule kieModule = kb.getKieModule();
+		KieContainer kContainer = kieServices.newKieContainer(kieModule.getReleaseId());
+		return kContainer;
+
+	}
+
+	private void getKieRepository() {
+		final KieRepository kieRepository = kieServices.getRepository();
+		kieRepository.addKieModule(new KieModule() {
+			public ReleaseId getReleaseId() {
+				return kieRepository.getDefaultReleaseId();
+			}
+		});
+	}
+
+	@Bean
+	public KieSession getKieSession() throws IOException {
+		return getKieContainer().newKieSession();
+
+	}
+
+}
